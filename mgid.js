618766f8b83e19fc6a9711f82afee2b541a2c5d1
Cafe Camfrog <cafecamfrog0@gmail.com>
2018-08-26 12:06:37 +0700

    Add files via upload

---
+++ mgid.js
@@ -0,0 +1,13 @@
+<!-- Composite Start -->
+<div id="M350632ScriptRootC270822">
+        <div id="M350632PreloadC270822">
+        Loading...    </div>
+        <script>
+                (function(){
+            var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
+            var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M350632ScriptRootC270822")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
+            catch(e){var iw=d;var c=d[gi]("M350632ScriptRootC270822");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=270822;c[ac](dv);
+            var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/c/a/cafecamfrog.blogspot.com.270822.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
+    </script>
+    </div>
+<!-- Composite End -->
